1a521072fd65e194ee109438e75ed206490f6aa1
supersergey <sergey.tolokunsky@gmail.com>
2015-08-27 17:12:23 +0300

    Spring/Hibernate AdProj

---
+++ Lesson07/AdProj/src/main/java/ua/kiev/prog/Main.java
@@ -0,0 +1,15 @@
+package ua.kiev.prog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by user on 27.08.2015.
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args)
+    {
+       SpringApplication.run(ua.kiev.prog.Main.class, args);
+    }
+}
