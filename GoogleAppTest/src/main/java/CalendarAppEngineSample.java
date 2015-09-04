8fe13feda9eb20694ee6b3c7c9979797eae84366
supersergey <sergey.tolokunsky@gmail.com>
2015-09-04 23:54:23 +0300

    Diploma

--- GoogleAppTest/src/main/java/CalendarAppEngineSample.java
+++ GoogleAppTest/src/main/java/CalendarAppEngineSample.java
@@ -1,12 +1,15 @@
 import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
 import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.auth.oauth2.TokenResponse;
 import com.google.api.client.extensions.appengine.auth.oauth2.AbstractAppEngineAuthorizationCodeServlet;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.appengine.api.users.UserServiceFactory;
 import com.google.gdata.client.spreadsheet.SpreadsheetService;
 import com.google.gdata.data.spreadsheet.SpreadsheetEntry;
 import com.google.gdata.data.spreadsheet.SpreadsheetFeed;
+import com.google.gdata.util.ContentType;
 import com.google.gdata.util.ServiceException;
 
 
@@ -15,6 +18,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URL;
 import java.util.List;
 
@@ -23,35 +27,55 @@ import java.util.List;
  */
 public class CalendarAppEngineSample extends AbstractAppEngineAuthorizationCodeServlet {
 
+    private AuthorizationCodeFlow flow;
+    Credential googleCredential;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
 
         try {
-            AuthorizationCodeFlow flow = initializeFlow();
-            Credential googleCredential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+            //AuthorizationCodeFlow flow = initializeFlow();
+
+            String ClientId = flow.getClientId();
+            Credential credential = flow.loadCredential(ClientId);
+            //Credential credential = new GoogleCredential().setAccessToken(accessToken);
+
+
+//            String userId = UserServiceFactory.getUserService().getCurrentUser().getUserId();
+//            Credential credential = Utils.getCredentials();
+
+
+
             SpreadsheetService service =
                     new SpreadsheetService("Diploma");
-            service.setOAuth2Credentials(googleCredential);
-            String accessToken = googleCredential.getAccessToken();
-            service.setAuthSubToken(accessToken);
+
+            credential.refreshToken();
+            service.setOAuth2Credentials(credential);
+
+
             URL SPREADSHEET_FEED_URL = new URL(
                     "https://spreadsheets.google.com/feeds/spreadsheets/private/full");
             SpreadsheetFeed feed = service.getFeed(SPREADSHEET_FEED_URL, SpreadsheetFeed.class);
             List<SpreadsheetEntry> spreadsheets = feed.getEntries();
             Sheets sheets = new Sheets();
+
+            response.setCharacterEncoding("UTF-8");
+            PrintWriter writer = response.getWriter();
+
             for (SpreadsheetEntry spreadsheet : spreadsheets) {
-                sheets.getSheets().add(new Sheet(spreadsheet.getTitle().getPlainText()));
+                //sheets.getSheets().add(new Sheet(spreadsheet.getTitle().getPlainText()));
+                writer.write(spreadsheet.getTitle().getPlainText()+"\r\n");
                 // Print the title of this spreadsheet to the screen
 //                        System.out.println(spreadsheet.getTitle().getPlainText());
             }
-            response.setCharacterEncoding("UTF-8");
+
             response.setStatus(HttpServletResponse.SC_OK);
             request.setAttribute("list", sheets);
             RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("results.jsp");
-            dispatcher.forward(request, response);
+            // dispatcher.forward(request, response);
         }
-        catch (ServletException | ServiceException ex)
+        catch (Exception ex)
         {
             ex.printStackTrace();
             response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
@@ -65,6 +89,7 @@ public class CalendarAppEngineSample extends AbstractAppEngineAuthorizationCodeS
 
     @Override
     protected AuthorizationCodeFlow initializeFlow() throws IOException {
-        return Utils.newFlow();
+        flow = Utils.newFlow();
+        return flow;
     }
 }
--- GoogleAppTest/src/main/java/Utils.java
+++ GoogleAppTest/src/main/java/Utils.java
@@ -2,6 +2,8 @@ import com.google.api.client.extensions.appengine.datastore.AppEngineDataStoreFa
 import com.google.api.client.extensions.appengine.http.UrlFetchTransport;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
@@ -10,9 +12,15 @@ import com.google.api.services.calendar.CalendarScopes;
 // import com.google.appengine.api.search.checkers.Preconditions;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by user on 03.09.2015.
@@ -30,6 +38,12 @@ public class Utils {
 
     private static GoogleClientSecrets clientSecrets = null;
 
+    private static final List<String> SCOPES = Arrays.asList(
+            "https://spreadsheets.google.com/feeds",
+            "https://docs.google.com/feeds",
+            "https://www.googleapis.com/auth/drive");
+
+
     static String getRedirectUri(HttpServletRequest req) {
         GenericUrl url = new GenericUrl(req.getRequestURL().toString());
         url.setRawPath("/oauth2callback");
@@ -38,7 +52,7 @@ public class Utils {
 
     static GoogleAuthorizationCodeFlow newFlow() throws IOException {
         return new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
-                getClientCredential(), Collections.singleton(CalendarScopes.CALENDAR)).setDataStoreFactory(
+                getClientCredential(), SCOPES).setDataStoreFactory(
                 DATA_STORE_FACTORY).setAccessType("offline").build();
     }
 
@@ -53,5 +67,25 @@ public class Utils {
         }
 
         return clientSecrets;
+    }
+
+    static GoogleCredential getCredentials() throws GeneralSecurityException,
+            IOException, URISyntaxException {
+        JacksonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+        HttpTransport httpTransport = GoogleNetHttpTransport
+                .newTrustedTransport();
+
+        String CLIENT_ID = "749895953376-d8muikbmv6okhaaa4cthg68g3pamimd1@developer.gserviceaccount.com";
+
+        URL fileUrl = Utils.class.getResource("/Diploma.p12");
+        GoogleCredential credential = new GoogleCredential.Builder()
+                .setTransport(httpTransport)
+                .setJsonFactory(JSON_FACTORY)
+                .setServiceAccountId(CLIENT_ID)
+                .setServiceAccountPrivateKeyFromP12File(
+                        new File(fileUrl.toURI()))
+                .setServiceAccountScopes(SCOPES).build();
+
+        return credential;
     }
 }
